5594f038fdff4eab0a37a6807dd7a24781a38ed5
Elli <78215127+ElliNow@users.noreply.github.com>
2022-02-25 16:34:44 +0300

    Add files via upload

---
+++ TeacherWebSite(origin)/TeacherWebSiteApp/TeacherWebSiteApp/Data/Models/Attachment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeacherWebSiteApp.Data.Models
+{
+    public class Attachment : Entity
+    {
+        public string Name { get; set; }
+        public string Link { get; set; }
+        public ContentType ContentType { get; set; }
+    }
+
+    public enum ContentType
+    {
+        Presentation,
+        Video,
+        MethodicalBook
+    }
+}
---
+++ TeacherWebSite(origin)/TeacherWebSiteApp/TeacherWebSiteApp/Data/PageModels/Contact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeacherWebSiteApp.Data.Models;
+
+namespace TeacherWebSiteApp.Data.PageModels
+{
+    public class Contact : Entity
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+        public string? Link { get; set; }
+        public byte[] Badge { get; set; }
+
+    }
+}
