3a77371a6d9c75e673ffc6612e44d4b746ffc7c6
Elia <78215127+ElliNow@users.noreply.github.com>
2022-02-25 16:45:45 +0300

    commit

---
+++ TeacherWebSite(origin)/TeacherWebSiteApp/TeacherWebSiteApp/Data/PageModels/ArcticleBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TeacherWebSiteApp.Data.Models;
+
+namespace TeacherWebSiteApp.Data.PageModels
+{
+    public class ArcticleBlock : Entity
+    {
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public byte[]? Image { get; set; }
+        public string? Video { get; set; }
+        
+        public Article Article { get; set; }
+        public int ArticleId { get; set; }
+
+    }
+}
---
+++ TeacherWebSite(origin)/TeacherWebSiteApp/TeacherWebSiteApp/Data/PageModels/Article.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TeacherWebSiteApp.Data.Models
+{
+    public class Article : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
