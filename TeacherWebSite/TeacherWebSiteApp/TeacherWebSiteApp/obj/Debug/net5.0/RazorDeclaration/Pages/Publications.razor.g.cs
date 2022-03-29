415e5ffa1de0057e0cfe367f7ae4bcf5969acd45
Elia <78215127+ElliNow@users.noreply.github.com>
2022-03-29 13:45:16 +0300

    commit

--- TeacherWebSite/TeacherWebSiteApp/TeacherWebSiteApp/obj/Debug/net5.0/RazorDeclaration/Pages/Publications.razor.g.cs
+++ TeacherWebSite/TeacherWebSiteApp/TeacherWebSiteApp/obj/Debug/net5.0/RazorDeclaration/Pages/Publications.razor.g.cs
@@ -141,7 +141,7 @@ using AntDesign;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 46 "C:\Users\Эля\Documents\GitHub\TeacherWebSite\TeacherWebSite\TeacherWebSiteApp\TeacherWebSiteApp\Pages\Publications.razor"
+#line 50 "C:\Users\Эля\Documents\GitHub\TeacherWebSite\TeacherWebSite\TeacherWebSiteApp\TeacherWebSiteApp\Pages\Publications.razor"
        
     List<TeacherWebSiteApp.Data.Models.Publication> publications;
 
